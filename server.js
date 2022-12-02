8a5ffd1bcda223a6fcc6826ff0e87bb168e5a4a6
史开乐 <csurfssecure@gmail.com>
2022-12-02 01:32:05 -0500

    api calls

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 // parse incoming string or array data
 app.use(express.urlencoded({extended: true}));
 // parse incoming JSON data
 app.use(express.json());
+//tells express specific folder to make available 
+app.use(express.static('public'));
 
 app.listen(PORT, ()=> {
     console.log(`API server on port ${PORT}`);;
@@ -118,4 +121,17 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/animals', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
